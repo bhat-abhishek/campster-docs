d8e68858b24f15e75a611d28d6f4eab0c2a5764b
Abhi-Bhat18 <abhishekbhat344@gmail.com>
2024-10-30 20:23:53 +0530

    base url added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from "@astrojs/starlight";
 
 export default defineConfig({
   site: "https://abhi-bhat18.github.io",
+  base : '/mailman-docs',
   integrations: [
     starlight({
       title: "Mailman",
